02a75124a2be17112c0a202e6e38decdfd77a78e
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2021-12-19 20:24:00 +0500

    some architecture components are added

---
+++ lib/src/data/model/group.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_shopping_list/src/core/app_constants.dart';
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: AppConstants.groupHiveId)
+class Group extends HiveObject{
+  
+}
